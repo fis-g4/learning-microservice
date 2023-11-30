b7ba435a5beca4386e249a14ab145879d44d2fcb
TomasCB24 <tcamerob@gmail.com>
2023-11-30 15:34:03 +0100

    FEAT: LINK GOOGLE CLOUD STORAGE DATABASE WITH MATERIAL MODEL

--- server/routes/materials.ts
+++ server/routes/materials.ts
@@ -1,8 +1,24 @@
 import express, { Request, Response } from 'express'
 import { Material } from '../db/models/material'
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
+import { Storage } from '@google-cloud/storage';
 import mongoose from 'mongoose'
 
 const router = express.Router()
+const storage = new Storage({
+    keyFilename: './GoogleCloudKey.json',
+  });
+
+const bucketName = 'materials-bucket';
+const bucket = storage.bucket(bucketName);
+
+const upload = multer({
+storage: multer.memoryStorage(),
+limits: {
+    fileSize: 5 * 1024 * 1024, // 5 MB
+},
+});
 
 router.get('/me', async (req: Request, res: Response) => {
     //Default Author/userId
@@ -33,33 +49,59 @@ router.get('/:id/users', async (req: Request, res: Response) => {
     return res.status(404).json({ error: 'Material not found' })
 })
 
-router.post('/', async (req: Request, res: Response) => {
-    const { title, description, price, file, type }: MaterialInputs = req.body
-
-    if (!title || !description || !price || !file || !type) {
+router.post('/', upload.single('file'), async (req: Request, res: Response) => {
+    try {
+      const { title, description, price, type }: MaterialInputs = req.body;
+  
+      if (!title || !description || !price || !req.file || !type) {
         return res.status(400).json({
-            error: 'Missing required fields (title, description, price, file, type)',
-        })
+          error: 'Missing required fields (title, description, price, file, type)',
+        });
+      }
+  
+      // TODO: Change to logged user
+      const author = new mongoose.Types.ObjectId('60d5ecb44b930ac130e82d7e')
+  
+      const blob = bucket.file(`${uuidv4()}-${req.file.originalname}`)
+      const blobStream = blob.createWriteStream({
+        metadata: {
+          contentType: req.file.mimetype,
+        },
+      });
+  
+      blobStream.on('error', (err) => {
+        console.error('Error al subir el archivo:', err)
+        res.status(500).json({ error: 'Error uploading file.' })
+      });
+  
+      blobStream.on('finish', async () => {
+        const publicUrl = `https://storage.googleapis.com/${bucketName}/${blob.name}`
+  
+        const newMaterial = Material.build({
+          title,
+          description,
+          price,
+          author,
+          purchasers: [],
+          file: publicUrl,
+          type,
+        });
+  
+        try {
+          const savedMaterial = await newMaterial.save()
+          res.status(201).json(savedMaterial)
+        } catch (error) {
+          console.error('Error al guardar el material en la base de datos:', error)
+          res.status(500).json({ error: 'Error saving material.' })
+        }
+      });
+  
+      blobStream.end(req.file.buffer)
+    } catch (error) {
+      console.error('Error en la solicitud:', error)
+      res.status(500).json({ error: 'Internal Server Error' })
     }
-
-    //TODO: Change to logged user
-    //const author = req.user ? res.user.id : null;
-    const author = new mongoose.Types.ObjectId('60d5ecb44b930ac130e82d7e')
-
-    const newMaterial = Material.build({
-        title,
-        description,
-        price,
-        author,
-        purchasers: [],
-        file,
-        type,
-    })
-
-    const savedMaterial = await newMaterial.save()
-
-    res.status(201).json(savedMaterial)
-})
+  })
 
 router.put('/:id', async (req: Request, res: Response) => {
     const material = await Material.findById(req.params.id)
@@ -67,24 +109,30 @@ router.put('/:id', async (req: Request, res: Response) => {
     if (!material) {
         return res.status(404).json({ error: 'Material not found' })
     }
+    // TODO: Check if user is owner in order to update the material
+    // const author = material.author
+    // const authenticatedUserId = req.user ? req.user.id : null;
+
+    
+    // if (authenticatedUserId !== author.toString()) {
+    //     return res.status(403).json({ error: 'Unauthorized: You are not the author of this material' });
+    // }
 
     const {
         title,
         description,
         price,
-        file,
         type,
         purchasers,
     }: MaterialInputs = req.body
 
-    if (!title && !description && !price && !file && !type && !purchasers) {
+    if (!title && !description && !price && !type && !purchasers) {
         return res.status(400).json({ error: 'No fields to update provided' })
     }
 
     if (title) material.title = title
     if (description) material.description = description
     if (price) material.price = price
-    if (file) material.file = file
     if (type) material.type = type
     if (purchasers) material.purchasers = purchasers
 
@@ -94,14 +142,34 @@ router.put('/:id', async (req: Request, res: Response) => {
 })
 
 router.delete('/:id', async (req: Request, res: Response) => {
-    //TODO: Check if user is the author of the material
-    const material = await Material.findById(req.params.id)
+    const material = await Material.findById(req.params.id);
     if (!material) {
-        return res.status(404).json({ error: 'Material not found' })
+        return res.status(404).json({ error: 'Material not found' });
     }
 
-    await material.deleteOne()
-    return res.status(200).send('Material deleted successfully')
-})
+    // TODO: Check if user is the author of the material
+    // const authenticatedUserId = req.user ? req.user.id : null;
+
+    // // Comparar el usuario autenticado con el autor del material
+    // if (authenticatedUserId !== material.author.toString()) {
+    //     return res.status(403).json({ error: 'Unauthorized: You are not the author of this material' });
+    // }
+
+    const fileUrl = material.file;
+    const fileName = fileUrl.split('/').pop();
+
+    try {
+        if (fileName !== undefined){
+            await bucket.file(fileName).delete();
+        }
+        
+    } catch (error) {
+        console.error('Error deleting file from bucket:', error);
+        return res.status(500).json({ error: 'Error deleting file from bucket' });
+    }
+
+    await material.deleteOne();
+    return res.status(200).send('Material deleted successfully');
+});
 
 export default router
