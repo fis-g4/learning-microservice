a7324d32e840bc34b968addbe5314491011ad559
TomasCB24 <tcamerob@gmail.com>
2023-12-09 00:00:06 +0100

    TEST: IMPLEMENT MATERIAL TESTS
    Co-authored-by: Miguel Romero Arjona <miguelromeroarjona@gmail.com>

--- server/routes/classes.ts
+++ server/routes/classes.ts
@@ -4,16 +4,15 @@ import mongoose from 'mongoose'
 
 const router = express.Router()
 
-router.get("/:id", async (req: Request, res: Response) => {
+router.get('/:id', async (req: Request, res: Response) => {
     //TODO: Check if user has access to class
-    
+
     const classData = await Class.findById(req.params.id)
     if (classData) {
         return res.status(200).json(classData)
     }
-    return res.status(404).json({ error: "Class not found" })
+    return res.status(404).json({ error: 'Class not found' })
 })
-
 
 //Reminder
 
@@ -27,12 +26,12 @@ this is responsible of another microservice
 
 //TODO: Connect to course microservice in frontend
 
-router.post("/", async (req: Request, res: Response) => {
+router.post('/', async (req: Request, res: Response) => {
     const { title, description, order, file }: ClassInputs = req.body
 
     if (!title || !description || !order || !file) {
         return res.status(400).json({
-            error: "Missing required fields (title, description, order, file)",
+            error: 'Missing required fields (title, description, order, file)',
         })
     }
 
@@ -48,49 +47,41 @@ router.post("/", async (req: Request, res: Response) => {
     return res.status(200).json(savedClass)
 })
 
-router.put("/:id", async (req: Request, res: Response) => {
+router.put('/:id', async (req: Request, res: Response) => {
     //TODO: Check if user is the author of the class
-    
-    
+
     const _class = await Class.findById(req.params.id)
 
     if (!_class) {
         return res.status(404).json({ error: 'Class not found' })
     }
 
-    const { 
-        title,
-        description, 
-        order,
-        file,
-    }: ClassInputs = req.body
+    const { title, description, order, file }: ClassInputs = req.body
 
     if (!title && !description && !order && !file) {
         return res.status(400).json({
-            error: 'No fields to update provided'
+            error: 'No fields to update provided',
         })
     }
 
-    
-    if(title) _class.title = title
-    if(description) _class.description = description
-    if(order) _class.order = order
-    if(file) _class.file = file
+    if (title) _class.title = title
+    if (description) _class.description = description
+    if (order) _class.order = order
+    if (file) _class.file = file
 
     const savedClass = await _class.save()
     return res.status(200).json(savedClass)
-    
 })
 
-router.delete("/:id", async (req: Request, res: Response) => {
+router.delete('/:id', async (req: Request, res: Response) => {
     //TODO: Check if user is the author of the class
 
     const classData = await Class.findById(req.params.id)
     if (classData) {
         await classData.deleteOne()
-        return res.status(200).json({ message: "Class deleted successfully" })
+        return res.status(200).json({ message: 'Class deleted successfully' })
     }
-    return res.status(404).json({ error: "Class not found" })
+    return res.status(404).json({ error: 'Class not found' })
 })
 
 export default router
